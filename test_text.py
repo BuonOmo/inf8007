d7e3e38fe1c90ea06e41732d896a9d05c5d24bb8
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-01-24 23:10:38 -0500

    update travis.yml (less versions), and typo fix

--- test_text.py
+++ test_text.py
@@ -5,26 +5,26 @@ from td1 import Text
 class TestText(TestCase):
     def test_count_words(self):
         t = lambda to_test: Text(to_test).count_words()
-        self.assertEqual(t('Bon/jour, mon/de!'), 2, "Simple case")
-        self.assertEqual(t('sau/te-mou/ton'), 2, "Composed word")
-        self.assertEqual(t(u"l'arbre"), 2, "Apostrophe separated words")
-        self.assertEqual(t(u"àéèâôûùê"), 1, "French stuff")
+        self.assertEqual(t("Bon/jour, mon/de!"), 2, "Simple case")
+        self.assertEqual(t("sau/te-mou/ton"), 2, "Composed word")
+        self.assertEqual(t("l'arbre"), 2, "Apostrophe separated words")
+        self.assertEqual(t("àéèâôûùê"), 1, "French stuff")
 
     def test_count_sentences(self):
         t = lambda to_test: Text(to_test).count_sentences()
-        self.assertEqual(t('Non.'), 1, "Simple case")
-        self.assertEqual(t('Non... Mais en fait si.'), 2, "...")
+        self.assertEqual(t("Non."), 1, "Simple case")
+        self.assertEqual(t("Non... Mais en fait si."), 2, "...")
 
     def test_count_syllables(self):
         t = lambda to_test: Text(to_test).count_syllables()
 
     def test_count_from_regex(self):
-        self.assertEqual(Text('abcde').count_from_regex(r'.'), 5, "Simple case is enough")
+        self.assertEqual(Text("abcde").count_from_regex(r"."), 5, "Simple case is enough")
 
     def test_process_lisibility(self):
         t = lambda to_test: Text(to_test).process_lisibility()
-        self.assertAlmostEqual(t('Tout est dit.'), 119.19, msg="Simple case is enought")
+        self.assertAlmostEqual(t("Tout est dit."), 119.19, msg="Simple case is enought")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
